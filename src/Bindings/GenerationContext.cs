a49bea64af0f7bff8893c61d6e3140dd8bac8953
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-04-06 21:46:17 +0900

    feat: add GenerationContext class and extend SchemaAttribute for property support

---
+++ src/Bindings/GenerationContext.cs
@@ -0,0 +1,43 @@
+﻿namespace Bindings;
+
+public sealed class GenerationContext
+{
+    /// <summary>
+    /// ViewModelAttributeの引数
+    /// </summary>
+    public readonly bool RequireBindImplementation;
+
+    /// <summary>
+    /// すでに<see cref="System.SerializableAttribute"/>を付与されているかどうか.
+    /// </summary>
+    public readonly bool AlreadySerializable;
+
+    /// <summary>
+    /// ModelAttributeが付与されたフィールド変数 or プロパティの型のフルネームと変数名（またはプロパティ名）
+    /// </summary>
+    public readonly (string typeFullName, string name)[] Models;
+
+    /// <summary>
+    /// SchemaAttributeが付与されたフィールド変数 or プロパティの引数情報.
+    /// </summary>
+    public readonly (string bindingPath, int id, string format) Schemas;
+
+    /// <summary>
+    /// SchemaAttributeが付与された関数の引数情報. Formatは不要なので除外で.
+    /// </summary>
+    public readonly (string bindingPath, int id) SchemaMethods;
+
+    public GenerationContext(
+        bool requireBindImplementation,
+        bool alreadySerializable,
+        (string typeFullName, string name)[] models,
+        (string bindingPath, int id, string format) schemas,
+        (string bindingPath, int id) schemaMethods)
+    {
+        RequireBindImplementation = requireBindImplementation;
+        AlreadySerializable = alreadySerializable;
+        Models = models;
+        Schemas = schemas;
+        SchemaMethods = schemaMethods;
+    }
+}
