02be80881e533d59e7061d9f3c07e75cd3158f88
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-04-10 03:25:27 +0900

    refactor: comment out unused CountViewModel and CountView classes for clarity

--- src/Bindings.Unity/Assets/Sample/Scripts/CountViewModel.cs
+++ src/Bindings.Unity/Assets/Sample/Scripts/CountViewModel.cs
@@ -37,83 +37,83 @@ namespace Bindings.Sample
     }
 
     // Planned to be generated auto.
-    [System.Serializable]
-    public sealed partial class CountViewModel : IViewModel
-    {
-        private readonly IMvvmPublisher _publisher;
-
-        public int Count
-        {
-            get => _count;
-            set
-            {
-                _count = value;
-                _publisher.PublishRebindMessage<CountViewModel>();
-            }
-        }
-
-        public CountViewModel(CountModel model, IMvvmPublisher publisher)
-        {
-            _model = model;
-            _publisher = publisher;
-        }
-
-        public void NotifyCompletedBind() => OnPostBind();
-
-        partial void OnPostBind();
-    }
+    // [System.Serializable]
+    // public sealed partial class CountViewModel : IViewModel
+    // {
+    //     private readonly IMvvmPublisher _publisher;
+    //
+    //     public int Count
+    //     {
+    //         get => _count;
+    //         set
+    //         {
+    //             _count = value;
+    //             _publisher.PublishRebindMessage<CountViewModel>();
+    //         }
+    //     }
+    //
+    //     public CountViewModel(CountModel model, IMvvmPublisher publisher)
+    //     {
+    //         _model = model;
+    //         _publisher = publisher;
+    //     }
+    //
+    //     public void NotifyCompletedBind() => OnPostBind();
+    //
+    //     partial void OnPostBind();
+    // }
 
     // Planned to be generated auto.
-    [System.Serializable]
-    public sealed partial class CountView : IView<CountViewModel>
-    {
-        [NonSerialized]
-        private CountViewModel _viewModel = null!;
+    // [System.Serializable]
+    // public sealed partial class CountView : IView<CountViewModel>
+    // {
+    //     [NonSerialized]
+    //     private CountViewModel _viewModel = null!;
+    //
+    //     [SerializeField]
+    //     private TMPro.TMP_Text _text = null!;
+    //
+    //     [SerializeField]
+    //     private UnityEngine.UI.Button _incrementButton = null!;
+    //
+    //     [SerializeField]
+    //     private UnityEngine.UI.Button _decrementButton = null!;
+    //
+    //     void IView<CountViewModel>.Initialize(CountViewModel viewModel)
+    //     {
+    //         _viewModel = viewModel;
+    //     }
+    //
+    //     System.Threading.Tasks.ValueTask IView.BindAsync(CancellationToken _)
+    //     {
+    //         BindAll();
+    //         return default;
+    //     }
+    //
+    //     private void BindAll()
+    //     {
+    //         _text.SetValue(_viewModel.Count);
+    //         _incrementButton.onClick.RemoveAllListeners();
+    //         _incrementButton.onClick.AddListener(_viewModel.Increment);
+    //         _decrementButton.onClick.RemoveAllListeners();
+    //         _decrementButton.onClick.AddListener(_viewModel.Decrement);
+    //         OnPostBind();
+    //         _viewModel.NotifyCompletedBind();
+    //     }
+    //
+    //     partial void OnPostBind();
+    // }
 
-        [SerializeField]
-        private TMPro.TMP_Text _text = null!;
-
-        [SerializeField]
-        private UnityEngine.UI.Button _incrementButton = null!;
-
-        [SerializeField]
-        private UnityEngine.UI.Button _decrementButton = null!;
-
-        void IView<CountViewModel>.Initialize(CountViewModel viewModel)
-        {
-            _viewModel = viewModel;
-        }
-
-        System.Threading.Tasks.ValueTask IView.BindAsync(CancellationToken _)
-        {
-            BindAll();
-            return default;
-        }
-
-        private void BindAll()
-        {
-            _text.SetValue(_viewModel.Count);
-            _incrementButton.onClick.RemoveAllListeners();
-            _incrementButton.onClick.AddListener(_viewModel.Increment);
-            _decrementButton.onClick.RemoveAllListeners();
-            _decrementButton.onClick.AddListener(_viewModel.Decrement);
-            OnPostBind();
-            _viewModel.NotifyCompletedBind();
-        }
-
-        partial void OnPostBind();
-    }
-
-#if UNITY_EDITOR || DEVELOPMENT_BUILD || !DISABLE_DEBUGTOOLKIT
-    public sealed partial class CountView : IMvvmSubscriber<global::Bindings.DebugBindMessage>
-    {
-        void IMvvmSubscriber<global::Bindings.DebugBindMessage>.OnReceivedMessage(global::Bindings.DebugBindMessage message)
-        {
-            message.BindTo(this);
-            _text.SetValue(_viewModel.Count);
-            OnPostBind();
-            _viewModel.NotifyCompletedBind();
-        }
-    }
-#endif
+// #if UNITY_EDITOR || DEVELOPMENT_BUILD || !DISABLE_DEBUGTOOLKIT
+//     public sealed partial class CountView : IMvvmSubscriber<global::Bindings.DebugBindMessage>
+//     {
+//         void IMvvmSubscriber<global::Bindings.DebugBindMessage>.OnReceivedMessage(global::Bindings.DebugBindMessage message)
+//         {
+//             message.BindTo(this);
+//             _text.SetValue(_viewModel.Count);
+//             OnPostBind();
+//             _viewModel.NotifyCompletedBind();
+//         }
+//     }
+// #endif
 }
